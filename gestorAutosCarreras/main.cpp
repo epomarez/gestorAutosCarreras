88ed795c1912298e3f727234a4d92216d070f807
Edrey Pomarez <pomarezedrey@gmail.com>
2025-03-27 22:12:17 -0600

    Update and improve file handling functions

--- gestorAutosCarreras/main.cpp
+++ gestorAutosCarreras/main.cpp
@@ -3,34 +3,35 @@
 #include <fstream>
 #include <cstdlib>
 #include <iostream>
+#include <optional>
 #include <regex>
 
 using namespace std;
 
 // Gestor autos
-const size_t filasAutos{7};
-const size_t columnasAutos{25};
-bool insertarAuto(const array<string, columnasAutos> &, string);
-string obtenerAuto(string);
-bool eliminarAuto(string);
-string modificarAuto(const array<array<char, 3>, 2> &);
+const size_t registrosAutos{25};
+const size_t columnasRegistro{7};
+bool eliminarAuto(const string);
 string generarCodigoAuto();
+array<string, 7> getAuto(const string, const string);
+string modificarAuto(const array<array<char, 3>, 2> &);
+bool setAuto(const array<string, columnasRegistro> &, const string);
 
 // Gestor competencias
 const size_t filasCompetencias{7};
 const size_t columnasCompetencias{25};
-bool insertarCompetencia(const array<array<char, columnasCompetencias>, filasCompetencias> &);
-string obtenerCompetencia(string);
-bool eliminarCompetencia(string);
-string modificarCompetencia(string, string[]);
+bool eliminarCompetencia(const string);
 string generarCodigoCompetencia();
+bool insertarCompetencia(const array<string, columnasRegistro> &, const string);
+string modificarCompetencia(const string, const string[]);
+string obtenerCompetencia(const string);
 
 // Validador entradas
 bool validarCodigoAuto(const string &);
-bool validarCodigoCompetencia(string);
+bool validarCodigoCompetencia(const string);
 bool validarFormatoFecha(const string &);
 bool validarNombre(string);
-bool validarNumeroRango(const string &, int, int);
+bool validarNumeroRango(const string &, const int, const int);
 bool validarSoloNumero(const string &);
 
 // Formateador Salida?
@@ -38,18 +39,38 @@ string formatearSalidaDatosAuto(string[]);
 string formatearSalidaDatosCompetencia(string[]);
 
 // Formateador Entrada?
-string formatearEntradaDatosAuto(const array<string, columnasAutos> &);
-string formatearEntradaDatosCompetencia(string[]);
+string formatearRegistro(const array<string, columnasRegistro> &);
+// string formatearEntradaDatosCompetencia(string[]);
 
 int main()
 {
     string direccionArchivoAutos = "Autos.txt";
     string direccionArchivoCompetencias = "Competencias.txt";
 
-    cout << "Hello world!" << endl;
-    bool resultado = insertarAuto({"Lore", "ipsum", "epomarez", "Ughst", "1234", "4321", "1111"}, direccionArchivoAutos);
-    cout << "holaaaaaaaaaaaaa" << resultado;
-    return 0;
+    ofstream autosArchivo("maravillas.txt", ios::out);
+    if (!autosArchivo)
+    {
+        cout << "Hubo un error";
+    }
+    else
+    {
+        cout << "Todo nice, ah!";
+    }
+
+    array<string, 7> registroAuto = getAuto("Edrey", direccionArchivoAutos);
+    if (registroAuto[0].empty())
+    {
+        cout << "Auto no encontrado" << endl;
+    }
+    else
+    {
+        cout << "Auto encontrado!" << endl;
+        for (int i = 0; i < columnasRegistro; i++)
+        {
+            cout << registroAuto[i];
+        }
+    }
+    system("pause");
 }
 
 // Validador entradas definición
@@ -89,7 +110,7 @@ bool validarFormatoFecha(const string &fecha)
     return true;
 }
 
-bool validarNombre(string nombre)
+bool validarNombre(const string nombre)
 {
     if (nombre.length() > 25)
     {
@@ -98,7 +119,7 @@ bool validarNombre(string nombre)
     return true;
 }
 
-bool validarNumeroRango(const string &numero, int max, int min)
+bool validarNumeroRango(const string &numero, const int max, const int min)
 {
     int num = stoi(numero);
     if (num > max || num < min)
@@ -122,10 +143,10 @@ bool validarSoloNumero(const string &numero)
     return true;
 }
 
-bool insertarAuto(const array<string, columnasAutos> &infoAuto, string nombreArchivo)
+bool setAuto(const array<string, columnasRegistro> &infoAuto, string direccionArchivo)
 {
 
-    ofstream appArchivoAutos(nombreArchivo, ios::app);
+    ofstream appArchivoAutos(direccionArchivo, ios::app);
     if (!appArchivoAutos)
     {
         cout << "Hubo un error, no se pudo abrir el archivo!" << endl;
@@ -136,15 +157,62 @@ bool insertarAuto(const array<string, columnasAutos> &infoAuto, string nombreArc
         cout << "El archivo se pudo abrir exitosamente!" << endl;
     }
 
-    string registroAuto = formatearEntradaDatosAuto(infoAuto);
+    string registroAuto = formatearRegistro(infoAuto);
 
     appArchivoAutos << registroAuto;
 
     return true;
 }
 
+array<string, 7> getAuto(const string codigo, const string direccionArchivo)
+{
+    array<string, 7> registroAuto;
+
+    fstream inArchivoAutos(direccionArchivo, ios::in);
+    if (!inArchivoAutos)
+    {
+        cerr << "No se pudo abrir el archivo" << endl;
+    }
+    else
+    {
+        cout << "El archivo se abrió correctamente" << endl;
+    }
+
+    while (inArchivoAutos >> registroAuto[0] >> registroAuto[1] >> registroAuto[2] >> registroAuto[3] >> registroAuto[4] >> registroAuto[5] >> registroAuto[6])
+    {
+        cout << registroAuto[1];
+        cout << "Hola" << endl;
+        if (registroAuto[0] == codigo)
+        {
+            return registroAuto;
+        }
+    }
+
+    return {""};
+}
+
+bool insertarCompetencia(const array<string, columnasRegistro> &infoCompetencia, string direccionArchivo)
+{
+    ofstream appArchivoAutos(direccionArchivo, ios::app);
+    if (!appArchivoAutos)
+    {
+        cout << "Hubo un error, no se pudo abrir el archivo!" << endl;
+        return false;
+    }
+    else
+    {
+        cout << "El archivo se pudo abrir exitosamente!" << endl;
+    }
+
+    string registroCompetencia = formatearRegistro(infoCompetencia);
+
+    appArchivoAutos << registroCompetencia;
+
+    return true;
+}
+
 // Formateador Entrada?
-string formatearEntradaDatosAuto(const array<string, columnasAutos> &infoAuto)
+string formatearRegistro(const array<string, columnasRegistro> &infoAuto)
 {
     string registro;
     for (const string &campo : infoAuto)
