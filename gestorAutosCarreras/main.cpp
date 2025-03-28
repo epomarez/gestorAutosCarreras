49448efa424ba0e42656b7c10547010781313de9
Edrey Pomarez <pomarezedrey@gmail.com>
2025-03-28 09:52:48 -0600

    Finish definition of setters and getters

--- gestorAutosCarreras/main.cpp
+++ gestorAutosCarreras/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 // Gestor autos
 const size_t registrosAutos{25};
 const size_t columnasRegistro{7};
-bool eliminarAuto(const string);
+bool eliminarAuto(const string, const string);
 string generarCodigoAuto();
 array<string, 7> getAuto(const string, const string);
 string modificarAuto(const array<array<char, 3>, 2> &);
@@ -19,12 +19,12 @@ bool setAuto(const array<string, columnasRegistro> &, const string);
 
 // Gestor competencias
 const size_t filasCompetencias{7};
-const size_t columnasCompetencias{25};
-bool eliminarCompetencia(const string);
+const size_t columnasRegistroCompetencia{7};
+// bool eliminarCompetencia(const string, const string);
 string generarCodigoCompetencia();
-bool insertarCompetencia(const array<string, columnasRegistro> &, const string);
+array<string, columnasRegistro> getCompetencia(const string, const string);
+bool setCompetencia(const array<string, columnasRegistro> &, const string);
 string modificarCompetencia(const string, const string[]);
-string obtenerCompetencia(const string);
 
 // Validador entradas
 bool validarCodigoAuto(const string &);
@@ -47,30 +47,14 @@ int main()
     string direccionArchivoAutos = "Autos.txt";
     string direccionArchivoCompetencias = "Competencias.txt";
 
-    ofstream autosArchivo("maravillas.txt", ios::out);
-    if (!autosArchivo)
+    array<string, 7> registroAuto = getCompetencia("Edreya", direccionArchivoAutos);
+    if (!registroAuto[0].empty())
     {
-        cout << "Hubo un error";
+        cout << formatearRegistro(registroAuto) << endl;
     }
     else
-    {
-        cout << "Todo nice, ah!";
-    }
 
-    array<string, 7> registroAuto = getAuto("Edrey", direccionArchivoAutos);
-    if (registroAuto[0].empty())
-    {
-        cout << "Auto no encontrado" << endl;
-    }
-    else
-    {
-        cout << "Auto encontrado!" << endl;
-        for (int i = 0; i < columnasRegistro; i++)
-        {
-            cout << registroAuto[i];
-        }
-    }
-    system("pause");
+        system("pause");
 }
 
 // Validador entradas definición
@@ -143,6 +127,7 @@ bool validarSoloNumero(const string &numero)
     return true;
 }
 
+// Gestor autos
 bool setAuto(const array<string, columnasRegistro> &infoAuto, string direccionArchivo)
 {
 
@@ -164,7 +149,7 @@ bool setAuto(const array<string, columnasRegistro> &infoAuto, string direccionAr
     return true;
 }
 
-array<string, 7> getAuto(const string codigo, const string direccionArchivo)
+array<string, columnasRegistro> getAuto(const string codigo, const string direccionArchivo)
 {
     array<string, 7> registroAuto;
 
@@ -180,18 +165,18 @@ array<string, 7> getAuto(const string codigo, const string direccionArchivo)
 
     while (inArchivoAutos >> registroAuto[0] >> registroAuto[1] >> registroAuto[2] >> registroAuto[3] >> registroAuto[4] >> registroAuto[5] >> registroAuto[6])
     {
-        cout << registroAuto[1];
-        cout << "Hola" << endl;
         if (registroAuto[0] == codigo)
         {
+            cout << "Auto encontrado" << endl;
             return registroAuto;
         }
     }
-
+    cout << "Auto no encontrado!" << endl;
     return {""};
 }
 
-bool insertarCompetencia(const array<string, columnasRegistro> &infoCompetencia, string direccionArchivo)
+// Gestor competencias
+bool setCompetencia(const array<string, columnasRegistro> &infoCompetencia, string direccionArchivo)
 {
     ofstream appArchivoAutos(direccionArchivo, ios::app);
     if (!appArchivoAutos)
@@ -209,6 +194,32 @@ bool insertarCompetencia(const array<string, columnasRegistro> &infoCompetencia,
     appArchivoAutos << registroCompetencia;
 
     return true;
+}
+
+array<string, columnasRegistro> getCompetencia(const string codigo, const string direccionArchivo)
+{
+    array<string, 7> registroCompetencia;
+
+    fstream inArchivoAutos(direccionArchivo, ios::in);
+    if (!inArchivoAutos)
+    {
+        cerr << "No se pudo abrir el archivo" << endl;
+    }
+    else
+    {
+        cout << "El archivo se abrió correctamente" << endl;
+    }
+
+    while (inArchivoAutos >> registroCompetencia[0] >> registroCompetencia[1] >> registroCompetencia[2] >> registroCompetencia[3] >> registroCompetencia[4] >> registroCompetencia[5] >> registroCompetencia[6])
+    {
+        if (registroCompetencia[0] == codigo)
+        {
+            cout << "Competencia encontrada!" << endl;
+            return registroCompetencia;
+        }
+    }
+    cout << "Competencia no encontrada!" << endl;
+    return {""};
 }
 
 // Formateador Entrada?
